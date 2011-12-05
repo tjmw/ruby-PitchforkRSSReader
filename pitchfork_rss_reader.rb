375709f378ba05e2264c82fbc905bcbc9a8f24c3
Tom Wey <tjmwey@gmail.com>
2011-12-05 20:44:50 +0000

    Parse results and store results

--- pitchfork_rss_reader.rb
+++ pitchfork_rss_reader.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
-
 require 'net/http'
 require 'uri'
 require 'rss/2.0'
+require 'htmlentities'
 
 module PitchforkRSSReader
 
@@ -10,11 +10,20 @@ module PitchforkRSSReader
 
         BASE_URL = 'http://feeds.feedburner.com/PitchforkBestNewAlbums?format=xml'
 
+        attr_reader :results
+
         def initialize
+            @results       = []
+
             fetch_content()
             parse_rss()
-            create_spotify_links()
+            build_results()
         end
+
+        private
+
+        attr_reader :raw_content
+        attr_reader :rss_content
 
         def fetch_content
             @raw_content = Net::HTTP.get(URI.parse(BASE_URL))
@@ -22,13 +31,50 @@ module PitchforkRSSReader
 
         def parse_rss
             @rss_content = RSS::Parser.parse(@raw_content, false)
-            puts @rss_content.channel.title
-            puts "#{@rss_content.items.size} items"
         end 
-        
-        private
+       
+        def build_results
+            @rss_content.items.each do |item|
+                artist, title = parse_artist_and_title(item.title)
 
-        attr_reader :raw_content
-        attr_reader :rss_content
+                @results << PitchforkRSSReader::Album.new(
+                    :title  => title,
+                    :artist => artist
+                )
+            end
+        end
+
+        def parse_artist_and_title(artist_and_title)
+
+            # strip leading/trailing whitespace
+            artist_and_title.gsub!(/(^\s+|\s+$)/, '') 
+
+            artist, title = artist_and_title.split(/\n-/)
+
+            if artist
+                artist.gsub!(/\n/, '') if artist
+                artist = HTMLEntities.new.decode(artist)
+            end
+
+            if title
+                title.gsub!(/\n/, '') if title
+                title = HTMLEntities.new.decode(title)
+            end
+
+            return artist, title
+        end
+
+    end
+
+    class PitchforkRSSReader::Album
+
+        attr_reader :title
+        attr_reader :artist
+
+        def initialize(args)
+            @title  = args[:title] 
+            @artist = args[:artist] 
+        end
+
     end
 end
